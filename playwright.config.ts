61035233004925eb4f4a784f2466dff8d161241b
vigneshravi2396 <vignesh.ravi@ideas2it.com>
2026-07-02 15:47:48 +0530

    added playwright file_1

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html', {outputFolder: 'playwright-report'}], ['json', {outputFile: 'test-results/results.json'}]] ,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -30,6 +30,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    video: 'on',
+    screenshot: 'on',
   },
 
   /* Configure projects for major browsers */
